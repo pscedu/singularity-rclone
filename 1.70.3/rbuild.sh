a962709f5e119273aa337665763e7cc2904d4590
Ivan Cao-Berg <icaoberg@andrew.cmu.edu>
2025-08-06 20:56:13 +0000

    Added version 1.70.3

---
+++ 1.70.3/rbuild.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Copyright © 2021-2025 Pittsburgh Supercomputing Center.
+# All Rights Reserved.
+
+IMAGE=singularity-rclone-1.70.3.sif
+DEFINITION=Singularity
+
+if [ -f $IMAGE ]; then
+	rm -fv $IMAGE
+fi
+
+singularity build --remote $IMAGE $DEFINITION
+
+if [ -f $IMAGE ]; then
+	exit 0
+else
+	exit 1
+fi
---
+++ 1.70.3/test.sh
@@ -0,0 +1,3 @@
+tree .
+
+singularity exec singularity-rclone-1.70.3.sif rclone --help
